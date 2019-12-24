037ca66a4294e31c272d5ff6d021d65f7619fe59
Thomas Rayden <thomasmwrayden@gmail.com>
2019-12-24 14:08:37 +0800

    Ignore system windows

--- src/main.rs
+++ src/main.rs
@@ -4,79 +4,82 @@ use std::io::Error;
 
 #[cfg(windows)]
 fn start_virus() -> Result<i32, Error> {
-    use winapi::shared::windef::POINT;
-    use winapi::um::winuser::{
-        GetAsyncKeyState, GetCursorPos, IsWindow, SetWindowPos, ShowWindow, WindowFromPoint,
-        HWND_TOP, SW_SHOWNORMAL, VK_MENU, VK_SHIFT,
-    };
+  use winapi::shared::windef::POINT;
+  use winapi::um::winuser::{
+    GetAsyncKeyState, GetCursorPos, GetWindowInfo, IsWindow, SetWindowPos, ShowWindow,
+    WindowFromPoint, HWND_TOP, SW_SHOWNORMAL, VK_MENU, VK_SHIFT, WINDOWINFO, WS_TILED,
+  };
 
-    let mut running = true;
+  let mut running = true;
 
-    let mut last_x = 0;
-    let mut last_y = 0;
+  let mut last_x = 0;
+  let mut last_y = 0;
 
-    while running {
-        unsafe {
-            if GetAsyncKeyState(VK_MENU) != 0 && GetAsyncKeyState(VK_SHIFT) != 0 {
-                running = false;
-            }
+  while running {
+    unsafe {
+      if GetAsyncKeyState(VK_MENU) != 0 && GetAsyncKeyState(VK_SHIFT) != 0 {
+        running = false;
+      }
 
-            let mut current_pos = POINT { x: 0, y: 0 };
+      let mut current_pos = POINT { x: 0, y: 0 };
 
-            GetCursorPos(&mut current_pos);
+      GetCursorPos(&mut current_pos);
 
-            let current_window = WindowFromPoint(current_pos);
+      let current_window = WindowFromPoint(current_pos);
 
-            if IsWindow(current_window) != 0 {
-                let curr_pos = &current_pos;
-                
-                let curr_x = curr_pos.x;
-                let curr_y = curr_pos.y;
+      let mut window_info = WINDOWINFO {};
 
-                let curr_x_distance = curr_x - last_x;
-                let curr_y_distance = curr_y - last_y;
+      GetWindowInfo(current_window, &mut window_info);
 
-                
-                let curr_x_offset = if curr_x_distance < 0 {
-                    return -10;
-                } else {
-                    return 10;
-                }
+      if IsWindow(current_window) != 0 && window_info.dwStyle == WS_TILED {
+        let curr_pos = &current_pos;
 
-                let curr_y_offset if curr_y_distance < 0 {
-                    return -10;
-                } else {
-                    return 10;
-                }
+        let curr_x = curr_pos.x;
+        let curr_y = curr_pos.y;
 
-                println!("x: {} y: {}", curr_pos.x, curr_pos.y);
-                
-                ShowWindow(current_window, SW_SHOWNORMAL);
+        let curr_x_distance = curr_x - last_x;
+        let curr_y_distance = curr_y - last_y;
 
-                SetWindowPos(
-                    current_window,
-                    HWND_TOP,
-                    curr_pos.x + curr_x_offset,
-                    curr_pos.y + curr_y_offset,
-                    curr_pos.x + 100,
-                    curr_pos.y + 100,
-                    0,
-                );
-            }
+        let curr_x_offset = if curr_x_distance < 0 {
+          return -10;
+        } else {
+          return 10;
+        };
 
-            last_x = curr_pos.x;
-            last_y = curr_pos.y;
-        }
+        let curr_y_offset = if curr_y_distance < 0 {
+          return -10;
+        } else {
+          return 10;
+        };
+
+        println!("x: {} y: {}", curr_pos.x, curr_pos.y);
+
+        ShowWindow(current_window, SW_SHOWNORMAL);
+
+        SetWindowPos(
+          current_window,
+          HWND_TOP,
+          curr_pos.x + curr_x_offset,
+          curr_pos.y + curr_y_offset,
+          curr_pos.x + 100,
+          curr_pos.y + 100,
+          0,
+        );
+      }
+
+      last_x = curr_pos.x;
+      last_y = curr_pos.y;
     }
+  }
 
-    return Ok(0);
+  return Ok(0);
 }
 
 #[cfg(not(windows))]
 fn start_virus() -> Result<(), Error> {
-    return Ok(());
+  return Ok(());
 }
 
 fn main() {
-    start_virus().unwrap();
+  start_virus().unwrap();
 }
